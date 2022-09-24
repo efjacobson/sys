aaea2a45e0c3fd8e1e247f462d333d11f60f8bc6
Eric Jacobson <contact@efjacobson.com>
2022-09-24 15:29:57 -0700

    ga

---
+++ common/bin/bash/ga.bash
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+main() {
+  local ignores=()
+  if [ 'WTMZ-TMZ006298' == "$(hostname)" ]; then
+    ignores+=('provisioning/docker/environment.sh')
+    ignores+=('provisioning/docker/vhost.conf.tpl')
+  fi
+
+  if [ "$1" != "" ]; then
+    git add "$1"
+  else
+    git add -A
+
+    for ignore in "${ignores[@]}"; do
+      if [ -f "$ignore" ]; then
+        git restore --staged "$ignore"
+      fi
+    done
+  fi
+}
+
+main "$@"
