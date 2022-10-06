4ea3a7eb28c7a7e0262a3ed3d7c984473d079275
Eric Jacobson <contact@efjacobson.com>
2022-10-06 02:26:17 -0700

    tmx

--- NeurAspire/bin/bash/t.bash
+++ NeurAspire/bin/bash/t.bash
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-tmux new-session "tmux source-file ~/.tmux.session.0"
+here=$(realpath "$0")
+src=
+while [ '' == "$src" ]; do
+    here=$(dirname "$here")
+    src=$(find "$here" -type f -name .tmux.session.0)
+done
+tmux new-session "tmux source-file $src"
