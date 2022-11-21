3600c44138e1122548cd5715767c6c077ba357af
Eric Jacobson <contact@efjacobson.com>
2022-11-21 14:06:33 -0800

    openlinks

--- WTMZ-TMZ006298/bin/bash/openlinks.bash
+++ WTMZ-TMZ006298/bin/bash/openlinks.bash
@@ -3,9 +3,10 @@
 append=''
 where='github'
 app='web'
+vendor='tmz'
 
 display_help() {
-    echo "
+  echo "
 Available options:
   --pull-requests
    -pr                If you want to open the list of pull requests
@@ -13,6 +14,8 @@ Available options:
    -b                 If you want to open the list of branches
   --releases
    -r                 If you want to open the list of releases
+  --labels
+   -l                 If you want to open the list of labels
   --where
    -w                 github (default) or cms
   --help
@@ -21,58 +24,62 @@ Available options:
 }
 
 for opt in "$@"; do
-    case ${opt} in
-    tmz | toofab) ;;
-
-    -pr | --pull-requests)
-        append='pulls/'
-        ;;
-    -b | --branch)
-        append='branches/'
-        ;;
-    -r | --releases)
-        append='releases/'
-        ;;
-    -f | --flagset)
-        append='releases/'
-        ;;
-    -w=* | --where=*)
-        where="${opt#*=}"
-        ;;
-    -a=* | --app=*)
-        app="${opt#*=}"
-        ;;
-    --help)
-        display_help
-        exit
-        ;;
-    *)
-        display_help
-        exit
-        ;;
-    esac
+  case ${opt} in
+  tmz | toofab)
+    vendor=$opt
+    shift
+    ;;
+  -pr | --pull-requests)
+    append='pulls/'
+    ;;
+  -b | --branch)
+    append='branches/'
+    ;;
+  -r | --releases)
+    append='releases/'
+    ;;
+  -l | --labels)
+    append='issues/labels/'
+    ;;
+  -f | --flagset)
+    append='releases/'
+    ;;
+  -w=* | --where=*)
+    where="${opt#*=}"
+    ;;
+  -a=* | --app=*)
+    app="${opt#*=}"
+    ;;
+  --help)
+    display_help
+    exit
+    ;;
+  *)
+    display_help
+    exit
+    ;;
+  esac
 done
 
 main() {
-    vendor="$1"
-    if [ 'cms' == "$where" ]; then
-        open -a "Brave Browser" -n --args --new-tab "https://cms.dev.$vendor.com/sys/flagsets/dev-$app"
-        open -a "Brave Browser" -n --args --new-tab "https://cms.dev.$vendor.com/sys/flagsets/local-$app"
-        open -a "Brave Browser" -n --args --new-tab "https://cms.test.$vendor.com/sys/flagsets/test-$app"
-        open -a "Brave Browser" -n --args --new-tab "https://cms.test.$vendor.com/sys/flagsets/local-$app"
-        open -a "Brave Browser" -n --args --new-tab "https://cms2.$vendor.com/sys/flagsets/prod-$app"
-        exit
-    fi
+  if [ 'cms' == "$where" ]; then
+    open -a "Brave Browser" -n --args --new-tab "https://cms.dev.$vendor.com/sys/flagsets/dev-$app"
+    open -a "Brave Browser" -n --args --new-tab "https://cms.dev.$vendor.com/sys/flagsets/local-$app"
+    open -a "Brave Browser" -n --args --new-tab "https://cms.test.$vendor.com/sys/flagsets/test-$app"
+    open -a "Brave Browser" -n --args --new-tab "https://cms.test.$vendor.com/sys/flagsets/local-$app"
+    open -a "Brave Browser" -n --args --new-tab "https://cms2.$vendor.com/sys/flagsets/prod-$app"
+    exit
+  fi
 
-    repos=(amp api cms feeds ncr pbjx share web)
-    for r in "${repos[@]}"; do
-        open -a "Brave Browser" -n --args --new-tab "https://github.com/tmz-apps/$vendor-$r/$append"
-    done
+  repos=(amp api cms feeds ncr pbjx share web)
+  for r in "${repos[@]}"; do
+    open -a "Brave Browser" -n --args --new-tab "https://github.com/tmz-apps/$vendor-$r/$append"
+  done
 
-    if [ 'tmz' == "$vendor" ]; then
-        open -a "Brave Browser" -n --args --new-tab "https://github.com/tmz-apps/$vendor-ovp/$append"
-        open -a "Brave Browser" -n --args --new-tab "https://github.com/tmz-apps/$vendor-app/$append"
-    fi
+  if [ 'tmz' == "$vendor" ]; then
+    open -a "Brave Browser" -n --args --new-tab "https://github.com/tmz-apps/$vendor-ovp/$append"
+    open -a "Brave Browser" -n --args --new-tab "https://github.com/tmz-apps/$vendor-app/$append"
+  fi
 }
 
 main "$@"
