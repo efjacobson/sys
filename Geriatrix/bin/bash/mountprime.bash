3d568030b89b1ac11ddb6d771ccabe6d6563f4e2
Eric Jacobson <contact@efjacobson.com>
2022-11-28 22:09:51 -0800

    common gcm

--- Geriatrix/bin/bash/mountprime.bash
+++ Geriatrix/bin/bash/mountprime.bash
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-sshfs Geriatrix@192.168.1.100:D:/ /home/sphynx/mnt/d
+me=$(whoami)
+sshfs Geriatrix@192.168.1.100:D:/ "/home/$me/mnt/d" -o "IdentityFile=/home/$me/.ssh/MountainPrime_id_ed25519"
--- NeurAspire/filesystem/home/sphynx/.profile
+++ NeurAspire/filesystem/home/sphynx/.profile
@@ -5,4 +5,4 @@ export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 
 ### begin mods ###
 
-export PATH=/home/sphynx/._/bin:$PATH
+export PATH=/home/"$(whoami)"/._/bin:$PATH
