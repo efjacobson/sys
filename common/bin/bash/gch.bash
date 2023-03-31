1f1083103c1969fceb490c6ef5737bc0fd07e68d
Eric Jacobson <contact@efjacobson.com>
2023-03-31 16:07:51 -0700

    morstorf

--- common/bin/bash/gch.bash
+++ common/bin/bash/gch.bash
@@ -1,43 +1,20 @@
 #! /bin/bash
 
-function remove() {
-  tmpdir="$1"
-  file_path="$2"
-
-  mv "$file_path" "$tmpdir/"
-  git checkout -q "$file_path"
-}
-
-function restore() {
-  tmpdir="$1"
-  dir="$2"
-  file_path="$3"
-
-  mv "$tmpdir/$file_path" "$dir/$file_path"
-}
-
 function main() {
+  if ! [[ $(git status --porcelain) ]]; then
+    echo 'already clean'
+    exit
+  fi
   if [ "$1" != "" ]; then
     echo "dont enter a file name chucklehead, this is for getting rid of everything"
   else
-    tmpdir="$HOME/.gch.tmp"
-    mkdir "$tmpdir"
-    environment_file_path=provisioning/docker/environment.sh
-    vhost_file_path=provisioning/docker/vhost.conf.tpl
-    if [ -f "$environment_file_path" ]; then
-      remove "$tmpdir" "$(grealpath "$environment_file_path")"
-    fi
-    if [ -f "$vhost_file_path" ]; then
-      remove "$tmpdir" "$(grealpath "$vhost_file_path")"
-    fi
-    git checkout -- .
-    if [ -f "$environment_file_path" ]; then
-      restore "$tmpdir" "$(dirname "$environment_file_path")" "$(basename "$environment_file_path")"
-    fi
-    if [ -f "$vhost_file_path" ]; then
-      restore "$tmpdir" "$(dirname "$vhost_file_path")" "$(basename "$vhost_file_path")"
-    fi
-    rm -rf "$tmpdir"
+    tmp="$(mktemp -d)"
+    for file in $(git diff --name-only); do
+      rsync -R "$file" "$tmp/"
+      git checkout -q "$file"
+    done
+    (sleep 300 && rm -rf "$tmp") &
+    echo "oopsie woopsie? the files with changes are in $tmp, they will be deleted in 5 minutes."
   fi
 }
 
