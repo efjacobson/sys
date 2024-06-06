80c994dc039abff92bf804f0963e932e4c3d1b1b
Eric Jacobson <contact@efjacobson.com>
2024-06-06 10:44:40 -0700

    for signing

--- common/bin/bash/gcm.bash
+++ common/bin/bash/gcm.bash
@@ -4,18 +4,23 @@ main() {
   local message="$*"
   [ -z "${message}" ] && message='wip'
 
+  gpgarg=''
+  if [ -x "$(command -v gpg)" ]; then
+    gpgarg='-S'
+  fi
+
   local toplevel && toplevel=$(git rev-parse --show-toplevel)
   if [[ $toplevel != *"tmz-apps"* ]]; then
-    git commit -m "$message"
+    git commit "${gpgarg}" -m "${message}"
     exit
   fi
 
   local branch && branch=$(git symbolic-ref --short HEAD)
   if [[ "$branch" =~ ^[0-9]+-.+$ ]]; then
     local ticket_number && ticket_number=$(echo "$branch" | awk -F'[^0-9]+' '{ print $1 }')
-    git commit -m "re: #$ticket_number $message"
+    git commit "${gpgarg}" -m "re: #${ticket_number} ${message}"
   else
-    git commit -m "$message"
+    git commit "${gpgarg}" -m "${message}"
   fi
 }
 
